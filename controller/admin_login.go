26a25a7e2e5b37a6b04c95c56db12d8102014cb9
湛慧平 <18702529695@163.com>
2021-07-09 22:51:57 +0800

    去掉一些注释

--- controller/admin_login.go
+++ controller/admin_login.go
@@ -2,13 +2,13 @@ package controller
 
 import (
 	"encoding/json"
+	"github.com/e421083458/golang_common/lib"
+	"github.com/gin-gonic/contrib/sessions"
+	"github.com/gin-gonic/gin"
 	"github.com/zhanhuipinggit/kingGataway/dao"
 	"github.com/zhanhuipinggit/kingGataway/dto"
 	"github.com/zhanhuipinggit/kingGataway/middleware"
 	"github.com/zhanhuipinggit/kingGataway/public"
-	"github.com/e421083458/golang_common/lib"
-	"github.com/gin-gonic/contrib/sessions"
-	"github.com/gin-gonic/gin"
 	"time"
 )
 
@@ -22,16 +22,6 @@ func AdminLoginRegister(group *gin.RouterGroup)  {
 
 }
 
-// AdminLogin godoc
-// @Summary 管理员登录
-// @Description 管理员登录
-// @Tags 管理员接口
-// @ID /admin_login/login
-// @Accept  json
-// @Produce  json
-// @Param polygon body dto.AdminLoginInput true "body"
-// @Success 200 {object} middleware.Response{data=dto.AdminLoginOutput} "success"
-// @Router /admin_login/login [post]
 func (adminlogin *AdminLogin) AdminLogin(c *gin.Context) {
 	params := &dto.AdminLoginInput{}
 	if err := params.BindValidParam(c); err !=nil {
@@ -73,15 +63,7 @@ func (adminlogin *AdminLogin) AdminLogin(c *gin.Context) {
 	middleware.ResponseSuccess(c,out)
 }
 
-// AdminLoginOut godoc
-// @Summary 管理员退出
-// @Description 管理员退出
-// @Tags 管理员退出
-// @ID /admin_login//login_out
-// @Accept  json
-// @Produce  json
-// @Success 200 {object} middleware.Response{data=string} "success"
-// @Router /admin_login/login_out [get]
+
 func (adminlogin *AdminLogin) AdminLoginOut(c *gin.Context) {
 
 	sess := sessions.Default(c)
